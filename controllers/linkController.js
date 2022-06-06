b487b8475fd5bc8972ae041bbad3b912dbfe7e91
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-06 17:35:18 -0400

    feat: add, delete e redirect links

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,10 +1,15 @@
 const Link = require('../models/Link');
 
-const redirectLink = async (req, res) => {
+const redirectLink = async (req, res, next) => {
+	let title = req.params.title;
 	try {
-		let title = req.params.title;
 		let doc = await Link.findOne({ title });
-		res.redirect(doc.url);
+
+		if (doc) {
+			res.redirect(doc.url);
+		} else {
+			next();
+		}
 	} catch (error) {
 		res.send(error);
 	}
@@ -15,10 +20,36 @@ const addLink = async (req, res) => {
 
 	try {
 		let doc = await link.save();
-		res.send(doc);
+		res.redirect('/');
+
+		alert('Link saved' + doc.title);
 	} catch (error) {
-		res.render('index', { error });
+		res.render('index', { error, body: req.body });
+	}
+};
+const allLinks = async (req, res) => {
+	try {
+		let links = await Link.find({});
+		res.render('all', { links: links });
+	} catch (error) {
+		res.send(error);
 	}
 };
 
-module.exports = { redirectLink, addLink };
+const deleteLink = async (req, res) => {
+	let id = req.params.id;
+
+	if (!id) {
+		id = req.body.id;
+	}
+
+	try {
+		await Link.findByIdAndDelete(id);
+		// res.send(id);
+		res.redirect('/');
+	} catch (error) {
+		res.status(404).send(error);
+	}
+};
+
+module.exports = { redirectLink, addLink, allLinks, deleteLink };
--- routes/Router.js
+++ routes/Router.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController');
+const methodOverride = require('method-override');
 
-router.get('/', (req, res) => {
-	res.render('index', { error: false });
-});
+router.use(methodOverride('_method'));
+
+router.get('/', linkController.allLinks);
+
 router.get('/:title', linkController.redirectLink);
+
+router.get('/add', (req, res) => {
+	res.render('index', { error: false, body: {} });
+});
 
 router.post(
 	'/',
 	express.urlencoded({ extended: true }),
 	linkController.addLink
 );
+
+router.delete(
+	'/',
+	express.urlencoded({ extended: true }),
+	linkController.deleteLink
+);
+
+router.delete('/:id', linkController.deleteLink);
 
 module.exports = router;
